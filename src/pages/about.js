c84ba64df50346fb8e780daafcfc30dffd62a65a
Matt Dioso <dioso.matt@gmail.com>
2024-08-12 22:03:34 -0700

    testing CI/CD with GitHub actions

--- src/pages/about.js
+++ src/pages/about.js
@@ -47,7 +47,7 @@ class AboutPage extends React.Component {
                 </div>
                 <div className="w-11/12 md:w-2/3 mx-4 mt-4 p-4 border border-white rounded-xl md:mx-auto">
                     <div className="w-full">
-                        <p className="text-white text-xl">Follow us on Instagram</p>
+                        <p className="text-white text-xl">Follow us on Instagram (testing)</p>
                     </div>
                     <IGFeed />
                 </div>
